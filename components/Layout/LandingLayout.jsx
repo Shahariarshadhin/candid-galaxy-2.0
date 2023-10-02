4c7b2bda53aa1269131f8317f17b25bd4378bba1
Shahariarshadhin <shadhin19shahriar@gmail.com>
2023-10-03 00:11:37 +0600

    project initialization

---
+++ components/Layout/LandingLayout.jsx
@@ -0,0 +1,14 @@
+import Footer from "./footer/Footer";
+import LandingNavbar from "./navbar/LandingNavbar";
+
+const LandingLayout = (props) => {
+  return (
+    <div className="min-h-screen bg-black text-white">
+      <LandingNavbar/>
+      <main>{props.children}</main>
+      <Footer/>
+    </div>
+  );
+};
+
+export default LandingLayout;
---
+++ components/Layout/navbar/LandingNavbar.jsx
@@ -0,0 +1,13 @@
+
+const LandingNavbar = () => {
+  return (
+    <div className="container mx-auto py-4">
+      <div className="flex justify-between">
+        <div>CANDID GALAXY</div>
+        <div>Upload Photo</div>
+      </div>
+    </div>
+  )
+}
+
+export default LandingNavbar
